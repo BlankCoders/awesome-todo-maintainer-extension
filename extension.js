6c97f20d848a2111ea6cb7c9feaed0f1e8543153
lostgirljourney <falgunisarkar526@gmail.com>
2022-02-13 03:02:39 +0530

    Feat:MVP done 😎

--- extension.js
+++ extension.js
@@ -130,6 +130,104 @@ const read = async (baseURL, gDBID) => {
 	}
 };
 
+const update = async (baseURL, gDBID) => {
+	if (!gDBID) {
+		const message = "Database not found.";
+		vscode.window.showErrorMessage(message);
+		await getDbId();
+	} else {
+		let res = await axios.get(`${baseURL}/readTodo?databaseID=${gDBID}`);
+
+		const rtodos = res.data.results.map((rtodo) => {
+			return {
+				label: rtodo.title,
+				detail: `${rtodo.priority} - ${rtodo.status}`,
+				description: rtodo.remarks,
+				pageID: rtodo.pageID,
+			};
+		});
+
+		const rtodo = await vscode.window.showQuickPick(rtodos, {
+			matchOnDescription: true,
+		});
+
+		if (rtodo) {
+			let title = await vscode.window.showInputBox({
+				ignoreFocusOut: true,
+				prompt: "Add a TO-DO title.",
+				value: rtodo.label,
+			});
+
+			let priority;
+			let status;
+			let priorities = new Array();
+			let statuses = new Array();
+			res.data.results.map((ptodo) => {
+				priorities.push(ptodo.priority);
+				statuses.push(ptodo.status);
+			});
+
+			priorities = [...new Set(priorities)];
+			statuses = [...new Set(statuses)];
+
+			const ptodo = await vscode.window.showQuickPick(priorities, {
+				matchOnDescription: true,
+				title: "Priority of TO-DO:",
+			});
+
+			if (ptodo) {
+				priority = ptodo;
+			} else {
+				return;
+			}
+
+			const stodo = await vscode.window.showQuickPick(statuses, {
+				matchOnDescription: true,
+				title: "Status of TO-DO:",
+			});
+
+			if (stodo) {
+				status = stodo;
+			} else {
+				return;
+			}
+
+			let remarks = await vscode.window.showInputBox({
+				ignoreFocusOut: true,
+				value: rtodo.description,
+				prompt: "Add remarks.",
+			});
+
+			const data = JSON.stringify({
+				pageID: rtodo.pageID,
+				title: title,
+				priority: priority,
+				status: status,
+				remarks: remarks,
+			});
+
+			const config = {
+				method: "put",
+				url: `${baseURL}/updateTodo`,
+				headers: {
+					"Content-Type": "application/json",
+				},
+				data: data,
+			};
+
+			axios(config)
+				.then(function () {
+					vscode.window.showInformationMessage("To-Do updated.");
+				})
+				.catch(function () {
+					vscode.window.showErrorMessage("Process failed.");
+				});
+		} else {
+			return;
+		}
+	}
+};
+
 const deletes = async (baseURL, gDBID) => {
 	if (!gDBID) {
 		const message = "Database not found.";
@@ -226,7 +324,21 @@ async function activate(context) {
 		}
 	);
 
-	context.subscriptions.push(configureDb, readDb, readTodo, createTodo);
+	let updateTodo = vscode.commands.registerCommand(
+		"awesome-todo-maintainer.updateTodos",
+		async function () {
+			await update(baseURL, gDBID);
+		}
+	);
+
+	context.subscriptions.push(
+		configureDb,
+		readDb,
+		readTodo,
+		createTodo,
+		updateTodo,
+		deleteTodo
+	);
 }
 
 function deactivate() {}
